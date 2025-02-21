e51cb7a77816daad2b0697b4529369fa5ddf525d
MatheusInacio32 <mateusinacio32@gmail.com>
2025-02-21 15:52:53 -0300

    tela de cadastro - parte 1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import '../styles/global.css';
 import Login from '../screens/login/login';
+import Signin from '../screens/signin/signin';
 
 export default function App() {
   return (
     <View className="flex-1">
-      <StatusBar style="auto" />
-      <Login /> 
+      <StatusBar style="auto" /> 
+      <Signin />
     </View>
   );
 }
---
+++ src/screens/signin/signin.tsx
@@ -0,0 +1,92 @@
+import { Text, TextInput, View, SafeAreaView, Image, Animated, Pressable } from 'react-native';
+import { useRef, useState } from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
+
+export default function Login() {
+
+  const scaleAnim = useRef(new Animated.Value(1)).current;
+  const [buttonPressed, setButtonPressed] = useState(false); 
+
+  // Reduz o botão ao pressionar
+  const handlePressIn = () => {
+    setButtonPressed(true); 
+    Animated.spring(scaleAnim, {
+      toValue: 0.90,
+      useNativeDriver: true,
+    }).start();
+  };
+  const handlePressOut = () => {
+    setButtonPressed(false); 
+    Animated.spring(scaleAnim, {
+      toValue: 1,
+      friction: 3,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  return (
+    <View className="flex-1 bg-white p-6 justify-center items-center">
+
+      
+      <View className="flex items-center">
+        <Image
+          source={require('../../assets/logo.png')}
+          style={{ width: 300, height: 150, marginBottom: 30 }}
+        />
+        <Text className="text-4xl font-bold text-black">Cadastre-se!</Text>
+      </View>
+
+      <View className="mt-8 w-full px-8">
+        <View className="area-texto">
+          <TextInput
+            className="campo-texto"
+            placeholder="Nome Completo"
+            placeholderTextColor="#6b7280"
+          />
+          <MaterialIcons name="account-circle" size={24} color="gray" />
+        </View>
+
+        <Text className="text-xl text-gray-700 mt-5">Senha</Text>
+        <SafeAreaView>
+          <View className="area-texto">
+            <TextInput
+              className="campo-texto"
+              placeholder="Digite sua senha"
+              placeholderTextColor="#6b7280"
+              secureTextEntry
+            />
+            <MaterialIcons name="remove-red-eye" size={24} color="gray" />
+          </View>
+        </SafeAreaView>
+      </View>
+
+      <View className="mt-8 w-full px-8">
+        <Animated.View style={{ transform: [{ scale: scaleAnim }] }}>
+          <Pressable
+            onPressIn={handlePressIn}
+            onPressOut={handlePressOut}
+            style={{
+              backgroundColor: buttonPressed ? '#FDE018' : '#FACC15', 
+              paddingVertical: 16,
+              borderRadius: 999,
+              shadowColor: '#000',
+              shadowOpacity: 0.2,
+              shadowOffset: { width: 0, height: 2 },
+              shadowRadius: 4,
+              elevation: 5,
+            }}
+          >
+            <Text className="text-center text-black text-xl font-bold">Entrar</Text>
+          </Pressable>
+        </Animated.View>
+      </View>
+
+      <Text className="mt-6 text-center text-gray-500">
+        Não tem conta?{' '}
+        <Text className="text-yellow-500 font-bold">
+          Crie agora!
+        </Text>
+      </Text>
+    </View>
+  );
+}
